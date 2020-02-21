3dbba11966c55ce4468846d970cad1df7b5e76d6
Andrzej <akorcz74@gmail.com>
2020-02-21 14:13:52 +0100

    added and modified some scripts

--- DevOpsAutDeploy/externalMethod.groovy
+++ DevOpsAutDeploy/externalMethod.groovy
@@ -1,8 +1,13 @@
 
-def concat( String... p ) {
+def concatenate(String... p) {
     String s = ""
     p.each { s += it + " " }
     return s
 }
+
+def isNullOrEmpty(String s) {
+    return !s?.trim()
+}
+
 
 return this;
